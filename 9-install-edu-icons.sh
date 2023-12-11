60a10b930c5c0a7fc53e75d27d3505adb0c03138
Erik Dubois <erik.dubois@gmail.com>
2023-12-11 06:29:53 +0100

    edu icons

--- 9-install-edu-icons.sh
+++ 9-install-edu-icons.sh
@@ -11,11 +11,12 @@
 #
 ##################################################################################################################
 
-sh 1-*
-sh 2-*
-sh 3-*
-sh 4-*
-sh 5-*
-sh 6-*
-sh 7-*
-sh 900-*
+git clone https://github.com/erikdubois/edu-surfn-numixs-blue/ /tmp/edu-surfn-numixs-blue
+
+mkdir $HOME/.icons
+mv /tmp/edu-surfn-numixs-blue/surfn-icons/* $HOME/.icons/
+
+
+echo "################################################################"
+echo "###################    icons installed     ######################"
+echo "################################################################"
